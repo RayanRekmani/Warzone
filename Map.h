a92b70a80b13450c32bb034add5005533a78d455
RayanRekmani <70661555+RayanRekmani@users.noreply.github.com>
2026-02-04 17:22:10 -0500

    Map header file

---
+++ Map.h
@@ -0,0 +1,115 @@
+//
+// Created by Rayan Rekmani on 2026-01-28.
+//
+
+#ifndef WARZONE_MAP_H
+#define WARZONE_MAP_H
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+using std::ostream;
+
+// Forward declaration since Territory has a Player* owner but Player is defined in Player.h
+class Player;
+
+//Represents a continent from the .map file.
+class Continent{
+    private:
+        friend ostream& operator<<(ostream&, const Continent&);
+    // ^^  uses friend as it allows access to continent name and country ownership bonus to be accessed by the cout <<
+        string name;
+        int bonus;
+    public:
+        Continent(); //default constructor
+        Continent(const Continent &c); //copy constructor
+        Continent(string name, int bonus);
+        Continent& operator =(const Continent& c); //assignment operator
+        string getName() const;
+        int getBonus() const;
+        void setName(string name);
+        void setBonus(int bonus);
+
+};
+
+class Territory{
+    private:
+        friend ostream& operator<<(ostream&, const Territory&);
+        Player* owner; //only one player can
+        Continent* continent;
+        int numOfArmies;
+        int tempNumOfArmies;
+        string name;
+        vector<Territory*> adjacentTerritories;
+        int x, y;
+
+    public:
+        Territory();
+        Territory(const Territory &t);
+        Territory(string name, int x, int y, Continent* continent);
+        Territory(Player* owner, Continent* continent, int numOfArmies, string name, vector<Territory*>, int x, int y);
+        Territory(Player *owner, Continent* continent, int numOfArmies, string name, int x, int y);
+        Territory& operator=(const Territory& t);
+        Player* getOwner();
+        Continent* getContinent();
+        int getNumOfArmies();
+        int getTempNumOfArmies();
+        string getName();
+        vector<Territory*> getAdjacentTerritories();
+        bool isAdjacentTerritory(Territory* territory);
+        int getX();
+        int getY();
+
+        void setOwner(Player* player);
+        void setContinent(Continent* continent);
+        void setNumOfArmies(int numOfArmies);
+        void setTempNumOfArmies(int tempNumOfArmies);
+        void setName(string name);
+        void setAdjacentTerritories(vector<Territory*> territories);
+        void setX(int x);
+        void setY(int y);
+
+        void addAdjacentTerritory(Territory* territory);
+};
+
+class Map{
+    private:
+        friend ostream& operator<<(ostream&, const Map&);
+        vector <Territory*> allTerritories;
+        vector <Continent*> allContinents;
+        void dfs(vector<string>* visitedTerritoriesNames, Territory* currentTerritory);
+        void dfs_continent(vector<string>* visitedTerritoriesNames, Territory* currentTerritory, Continent* currentContinent);
+
+    public:
+        Map();
+        ~Map();
+        Map(vector<Territory*> territories, vector<Continent*> continents);
+        Map(const Map &map);
+        Map& operator=(const Map &m);
+
+        vector<Territory*> getAllTerritories();
+        vector<Continent*> getAllContinents();
+        vector<Territory*> getAllTerritoriesByContinent(Continent* continent);
+        void setAllTerritories(vector<Territory*> territories);
+        void setAllContinents(vector<Continent*> continents);
+
+        void addTerritory(Territory* t);
+        void addContinent(Continent* c);
+        bool isConnectedTerritories();
+        bool isConnectedContinents();
+        bool validate();
+};
+
+class MapLoader{
+    public:
+        MapLoader();
+        Map* readMapFile(string filepath);
+
+    private:
+        Continent* getContinentByNameFromSet(vector<Continent*> continents, string continentName);
+        Territory* getTerritoryByNameFromSet(vector<Territory*> territories, string territoryName);
+
+};
+
+
+#endif //WARZONE_MAP_H
