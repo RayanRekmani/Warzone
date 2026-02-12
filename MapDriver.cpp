001733c6c66fd72e3c6b66cf92cb0b6ad8d5be41
RayanRekmani <70661555+RayanRekmani@users.noreply.github.com>
2026-02-12 16:47:02 -0500

    Implement map loading and validation test
    This file contains a test function for loading and validating various map files using the MapLoader class. It demonstrates the loading process for five different maps and ensures their validity.

---
+++ MapDriver.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Rayan Rekmani on 2026-02-11.
+//
+
+#include <iostream>
+#include "MapDriver.h"
+#include "Map.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+void testLoadMap(){
+
+    MapLoader maploader;
+
+    string map_brasil = "brasil.map";
+    string map_euro = "europe.map";
+    string map_solar = "solar.map";
+    string map_google = "google.map";
+    string map_europass = "europass.map";
+
+    cout << "Loading Brasil Map" << endl;
+    Map* m1 = maploader.loadMap(map_brasil);
+    m1->validateMap();
+    cout << "----------------------------------------------" <<endl;
+    cout << endl;
+
+    cout << "Loading Euro Map" << endl;
+    Map* m2 = maploader.loadMap(map_euro);
+    m2->validateMap();
+    cout << "----------------------------------------------" <<endl;
+    cout << endl;
+
+    cout << "Loading Solar Map" << endl;
+    Map* m3 = maploader.loadMap(map_solar);
+    m3->validateMap();
+    cout << "----------------------------------------------" <<endl;
+    cout << endl;
+
+    cout << "Loading Google Map" << endl;
+    Map* m4 = maploader.loadMap(map_google);
+    m4->validateMap();
+    cout << "----------------------------------------------" <<endl;
+    cout << endl;
+
+    cout << "Loading Europass Map" << endl;
+    Map* m5 = maploader.loadMap(map_europass);
+    m5->validateMap();
+    cout << "----------------------------------------------" <<endl;
+    cout << endl;
+
+    delete m1;
+    delete m2;
+    delete m3;
+    delete m4;
+    delete m5;
+}
